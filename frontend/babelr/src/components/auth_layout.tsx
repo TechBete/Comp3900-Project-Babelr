f213b8c99416825fb9f5cbd36b8aaddc7e7e19a2
Nick <nifkode@gmail.com>
2025-03-17 09:46:20 +1100

    moved files

--- frontend/babelr/src/components/auth_layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-    return (
-      <div className="auth-container">
-        <main>{children}</main>
-      </div>
-    );
-  }
