89aa67ae4ffc56ff18782b4a7f7188f71e0059ba
Nick <nifkode@gmail.com>
2025-03-23 10:27:25 +1100

    removed search bar from navbar

--- frontend/babelr/components/nav_bar.tsx
+++ frontend/babelr/components/nav_bar.tsx
@@ -7,9 +7,7 @@ export default function Navbar() {
         <div className={styles["nav-bar"]}>
             <HomeIcon/>
             <div className={styles["top-nav-right"]}>
-                <div className={styles["search-bar"]}>
-                    <input type="text" placeholder="Search" />
-                </div>
+
                 <div className={styles["profile-options"]}>
                     <span>My Profile</span>
                     <span><Link href='/login'>Logout</Link></span>
