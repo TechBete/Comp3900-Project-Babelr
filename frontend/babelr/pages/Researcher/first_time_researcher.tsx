2bb224fcec8476a2b21af5ed5afd8a9eba6fd8c5
Nick <nifkode@gmail.com>
2025-03-23 10:11:30 +1100

    prototype for researcher first time page

---
+++ frontend/babelr/pages/Researcher/first_time_researcher.tsx
@@ -0,0 +1,94 @@
+import styles from "stylesheets/first_time_listener.module.css"
+import Image from "next/image";
+import { FormEvent, useState } from "react";
+
+export default function First_time() {
+    const [Error, setError] = useState("");
+
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget);
+        console.log(Object.fromEntries(formData.entries()));
+
+        for (const [key, value] of formData.entries()) {
+            // console.log(`${key}: ${value}`);
+            PostDemographics(`${key}`, `${value}`,`edit${key}`)
+        }  
+        console.log(Error)
+    }
+
+    async function PostDemographics(key: string, value: string ,API: string) {
+        console.log("DEMOGRAPHICS POST BELOW");
+        console.log(JSON.stringify({[key]: value}));
+        try {
+            const response = await fetch(`http://localhost:8016/${API}}`, {
+                method:"POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({[key]: value}),
+                credentials: 'include'
+            })
+    
+            if (response.ok) {
+                // const response = await response.json()
+                return // change later maybe
+            } else {
+                const error = await response.json();
+                setError(error.error);
+            }
+    
+        } catch {
+            setError("Network Error: Fetch Request Failed");
+        }        
+    }
+
+
+    return (
+        <div className={styles["body"]}>
+            <div className={styles["form-container"]}>
+                <div className={styles["information-container"]}>
+                    <div className={styles["header-wrapper"]}>
+                        <h1>Welcome to Babelr</h1>
+                        <p>Please fill in the information below</p>
+                        <form className={styles["demographics-forms"]} method="post" onSubmit={handleSubmit}>
+                            <label htmlFor="first-name">First Name</label>
+                            <input 
+                                className={styles.input}
+                                type="text" 
+                                id="first-name" 
+                                name="first-name" 
+                            required
+                            />
+
+                            <label htmlFor="last-name">Last Name</label>
+                            <input 
+                                className={styles.input}
+                                type="text" 
+                                id="last-name" 
+                                name="last-name" 
+                                required
+                            />
+                            
+                            <label htmlFor="country">Organisation</label> 
+                            <select 
+                                className={styles.select} 
+                                id="country" 
+                                name="country" 
+                                required
+                            >       
+                                <option value="" disabled >Select Your Organisation</option>
+                                <option>UNSW</option>
+                                <option>USYD</option>
+                                <option>CSIRO</option>
+                            </select>
+
+                            <button className={styles.button}type="submit">Next</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div className={styles["image-container"]}>
+                    <Image src="/babelr_logo.png" alt="side-image" width={1024} height={1024}/>
+            </div>
+        </div>
+   );
+}
