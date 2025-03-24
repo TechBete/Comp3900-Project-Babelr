87e83d393e3f682e407f3de1e7a88fb462dbbfec
Kevin Yu <kevin@DESKTOP-M3BVDVP>
2025-03-25 01:01:00 +1100

    backend route change

--- frontend/babelr/pages/file_upload.tsx
+++ frontend/babelr/pages/file_upload.tsx
@@ -48,7 +48,7 @@ export default function FileUploadPage() {
         formData.append("tags", tags);
 
         try {
-            const response = await fetch("http://localhost:8016/upload", {
+            const response = await fetch("http://localhost:8016/uploadAudioFile", {
                 method: "POST",
                 body: formData,
             });
