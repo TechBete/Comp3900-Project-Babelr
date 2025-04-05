a0a2fb4861bced8675504c818a562cc5e5af6aef
Nick <nifkode@gmail.com>
2025-04-06 01:18:49 +1100

    split nav bars for each user role

---
+++ frontend/babelr/components/nav_bar_listener.tsx
@@ -0,0 +1,19 @@
+import styles from "../stylesheets/nav_bar.module.css"
+import HomeIcon from "./homeicon"
+import Link from "next/link"
+
+export default function Navbar() {
+    return ( 
+        <div className={styles["nav-bar"]}>
+            <Link href='/Listener/clip_list'><HomeIcon/></Link>
+            <div className={styles["top-nav-right"]}>
+
+                <div className={styles["profile-options"]}>
+                    <span>Points</span>
+                    <span><Link href='/Listener/profile'>My Profile</Link></span>
+                    <span><Link href='/'>Logout</Link></span>
+                </div>
+            </div>
+        </div>
+    )
+} 
