6717b9cb167d7ddd235be9aea9fa6373a2408a52
Dysthesis <antheoraviel@protonmail.com>
2025-04-07 19:35:57 +1000

    Temp push

--- backend/matching.py
+++ backend/matching.py
@@ -1,6 +1,7 @@
 from enum import Enum
-from typing import Set
+from typing import List, Set
 from server import Listener, ProficiencyLevel
+import random
 
 class FilterMode(Enum):
     AND = "AND"
@@ -21,22 +22,42 @@ def filter_listeners(
     Returns:
         A set of UUIDs of users who meet the requirements.
     """
-    # Define a lambda to encapsulate the filter condition
-    meets_proficiency = lambda lang, proficiency: ProficiencyLevel.from_str(
-        Listener.languages[lang].astext
-    ) >= proficiency.value
-
     query = Listener.query
 
     if mode == FilterMode.AND:
         # Chain the filters for all language-proficiency pairs
         for lang, proficiency in language_requirements.items():
-            query = query.filter(meets_proficiency(lang, proficiency))
+            query = query.filter(
+                ProficiencyLevel.from_str(Listener.languages[lang].astext) >= proficiency.value
+            )
         filtered_users = set(query(Listener.id))
     else:
         # Accumulate results for each condition to cover the OR mode
         filtered_users = set()
         for lang, proficiency in language_requirements.items():
-            filtered_users.update(query(Listener.id).filter(meets_proficiency(lang, proficiency)))
+            filtered_users.update(query(Listener.id).filter(
+                ProficiencyLevel.from_str(Listener.languages[lang].astext) >= proficiency.value
+            ))
             
     return filtered_users
+
+def alloc_listeners(num_listeners: int, language_requirements: dict[str, ProficiencyLevel]) -> Set[str]:
+    """
+    Get `num_listeners` random listeners given a minimum language requirement.
+
+    Args:
+        num_listeners: the number of listeners to allocate
+        language_requirements: 
+
+    Returns:
+        a set of listener UUIDs
+    """
+    listeners = filter_listeners(language_requirements)
+
+    selected = set()
+
+    while len(selected) < num_listeners:
+        listener = random.choice(list(listeners))
+        selected.add(listener)
+
+    return selected;
