9c11d7b2b1c4d594cc32d6a78ec778715f70e64e
z5420287 <z5420287@ad.unsw.edu.au>
2025-04-09 15:24:39 +1000

    added points persistence across all listner pages, updated profile with navbar

--- frontend/babelr/pages/Listener/profile.tsx
+++ frontend/babelr/pages/Listener/profile.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 export default function ClipList() {
     return (
         <RoleCheck requiredRole="listener">
+            <Navbar/>
             <div style={{display: 'flex', justifyContent: 'center' , alignItems: 'center', height: '100vh', fontWeight: 'bold'}}>
                 THIS IS A TEMP LISTENER PROFILE PAGE TO TEST ROUTING.
                 <Link style={{textDecoration: 'underline', color: 'blue'}}href='/Listener/clip_list'>Back to Home</Link>
