0b8462c3ac2c25db3b6ac6932fefd43e7d90d096
Jeremy Fong <z5410231@ad.unsw.edu.au>
2025-04-21 11:22:49 +1000

    previous commit was corrupt and removed the added files

---
+++ frontend/babelr/pages/Researcher/project_list.tsx
@@ -0,0 +1,151 @@
+import Link from "next/link";
+import styles from "stylesheets/projects_list.module.css"
+import Navbar from "components/nav_bar_researcher";
+import Modal from "components/popout_modal";
+import CreateButton from "components/create_button";
+import { FormEvent, useEffect, useState } from "react";
+import RoleCheck from "components/role_checker";
+
+
+
+
+function Project({ project_name, path, status, creator_name } : { project_name: string; path: string; status: string; creator_name: string }) {
+    console.log('status is', status);
+    const StatusStyle = () => {
+        if (status == "in-progress") {
+            return <td className={styles["td"]}><span className={`${styles.status} ${styles["in-progress"]}`}>In Progress</span></td>
+        }  else if (status == "draft") {
+            return <td className={styles["td"]}><span className={`${styles.status} ${styles["draft"]}`}>Draft</span></td>
+        }
+    };
+
+    return (
+        <tr className={styles["tr"]}>
+            <td className={styles["td"]}><Link className={styles["projects-link"]} href={`${path}/audioclips`}>{project_name}</Link></td>
+            {StatusStyle()}
+            <td className={styles["td"]}>{creator_name}</td>
+        </tr>
+    );
+}
+
+function ProjectList({ projects }: { projects: { project_name: string; path: string; status: string; creator_name: string }[] }) {
+    return (
+        <tbody>
+            {projects.map((project_dict, index) => (
+                <Project key={index} {...project_dict} />
+            ))}
+        </tbody>
+    );
+}
+
+
+
+export default function MainScreen() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [projectName, setProjectName] = useState(""); // change this to empty string later for production
+    const [createError, setCreateError] = useState("");
+    const [projectsData, setProjectData] = useState([]);
+
+    useEffect(() => {
+        getProjects();
+    }, [])
+    async function getProjects() {
+        try {
+            const response = await fetch('http://localhost:8016/projects/getProjects' , {
+                method:"GET",
+                credentials: 'include'
+            })
+            if (response.ok) {
+                const project_list = await response.json();
+                setProjectData(project_list.projects_list);
+                console.log(project_list)
+            } else {
+                const error = await response.json()
+                console.log(error)
+            }
+        } catch {
+
+        }
+    } 
+
+    async function handleCreate(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        try {
+            const response = await fetch('http://localhost:8016/projects/createProject', {
+                method:"POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({"project_name": projectName}),
+                credentials: 'include'
+            })
+        
+            if (response.ok) {
+				const data = await response.json();
+                console.log(data.message);
+                getProjects();
+			} else {
+                const error = await response.json()
+				console.log("NOT OK");
+                console.log(error.error);
+			}
+        } catch {
+            setCreateError("Network Error: Fetch Request Failed");
+        }
+    }
+
+    return (
+        <RoleCheck requiredRole="researcher">
+            <div className={styles["project-body"]}>
+                < Navbar/>
+                <div className={styles["container"]}>
+                    <div className={styles["main-content"]}>
+                        <div className={styles["main-top"]}>
+                            <h2 className={styles["projects-h2"]}>
+                                Projects     
+                            </h2>
+                            <div className={styles["search-bar"]}>
+                                <input type="text" placeholder="Search" />
+                            </div>
+                        </div>
+                        <table className={styles["table"]} >
+                            <thead className={styles["thead"]}>
+                                <tr className={styles["tr"]}>
+                                    <th className={styles["th"]}>Name</th>
+                                    <th className={styles["th"]}>Status</th>
+                                    <th className={styles["th"]}>Creator</th>
+                                    <th className={styles["th"]}></th>
+                                </tr>
+                            </thead>
+                            <ProjectList projects={projectsData}/>
+                        </table>
+
+                        <CreateButton onClick={() => setIsModalOpen(true)}></CreateButton>
+                        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} hasCloseBtn>
+                            <h2 className={styles.modalTitle}>New Project</h2>
+
+                            <form onSubmit={handleCreate}>
+                                <label htmlFor="project_name" className={styles.formLabel}>
+                                    Project Name
+                                </label>
+                                <input
+                                    id="project_name"
+                                    type="text"
+                                    name="project_name"
+                                    value={projectName}
+                                    onChange={(e) => setProjectName(e.target.value)}
+                                    className={styles.inputField}
+                                />
+                                <button type="submit" className={styles.submitButton}>
+                                    Create Project
+                                </button>
+                                <div className={styles["invalid-label"]}>
+                                { createError !== "" && <div>{createError}</div>}
+                                </div>
+                            </form>
+                        </Modal>
+                    </div>
+                </div>
+            </div>
+        </RoleCheck>
+    );
+}
---
+++ frontend/babelr/pages/app/audioData/[userId]/[projectName]/audioclips.tsx
@@ -0,0 +1,277 @@
+import { useState, FormEvent, ChangeEvent, useEffect } from "react";
+import { useRouter } from 'next/router';
+import Navbar from "components/nav_bar_researcher";
+// import Sidebar from "components/side_bar";
+import Sidebar from "components/project_sidebar"
+import Modal from "components/popout_modal";
+import styles from "stylesheets/file_upload.module.css";
+// import AudioClipsTable from "components/clips_table";
+import {Box, Button} from "@mui/material";
+
+import Table from "components/table";
+import RoleCheck from "components/role_checker";
+
+interface AudioData {
+    file_name: string;
+    tags: string[];
+    model: string;
+    language: string;
+    dateAdded: string;
+    evaluated: string;
+    rating: number;
+}
+
+type RawAudioClip = {
+    file_name: string;
+    tags: string; // update this based on actual structure if needed
+    Researcher: string;
+    file_extension: string;
+    file_path: string;
+    allocated_listeners: never[];
+    project_name: string;
+    project_path: string;
+    model: string;
+    language: string;
+};
+
+
+enum LangProf {
+    none = 'none_set',
+    elementary = 'elementary',
+    limited_working = 'limited_working',
+    professional = 'professional',
+    native = 'native',
+    bilingual = 'bilingual',
+}
+
+
+export default function FileUploadPage() {
+    const router = useRouter();
+    const { projectName } = router.query; //  project name
+    console.log(projectName)
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [file, setFile] = useState<File | null>(null);
+    const [fileName, setFileName] = useState("");
+    const [tags, setTags] = useState("");
+    const [model, setModel] = useState("");
+    const [language, setLanguage] = useState("");
+    const [langProf, setLangProf] = useState<LangProf>(LangProf.none)
+    const [uploadError, setUploadError] = useState("");
+    const [audioData, setAudioData] = useState<AudioData[]>([]);
+    const [isStarted, setIsStarted] = useState(false);
+    
+
+    useEffect(() => {
+        if (typeof projectName === 'string') {
+            getAudioClips();
+        }
+    },[projectName] ); // <- only runs when projectName changes
+
+    if (typeof projectName !== 'string') {
+        return <div>Loading?</div>;
+    }
+
+    async function getAudioClips() {
+        console.log('project name is ',JSON.stringify({project_name: projectName}))
+        try {
+            const response = await fetch('http://localhost:8016/audio/getProjectAudioFiles' , {
+                method:"POST",
+                credentials: 'include',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({project_name: projectName}),
+            })
+            
+            if (response.ok) {
+                const res  = await response.json();
+                const audio_files = res.audio_files;
+                console.log('raw clips are', audio_files as RawAudioClip);
+                const formattedClips: AudioData[] = (audio_files as RawAudioClip[]).map(clip => ({
+                    file_name: clip.file_name,
+                    tags: (clip.tags as string)
+                        .split(',')
+                        .map(tag => tag.trim())
+                        .filter(Boolean),
+                    model: clip.model,
+                    language:clip.language,
+                    dateAdded: new Date().toLocaleDateString(),
+                    evaluated: "0/50",
+                    rating: 0,
+                }));
+                console.log('formatted audioClips:', formattedClips);
+                setAudioData(formattedClips);
+                console.log('clips are',formattedClips)
+            } else {
+                const error = await response.json()
+                console.log(error)
+            }
+        } catch {
+
+        }
+    } 
+    
+
+    function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
+        if (event.target.files && event.target.files.length > 0) {
+            const selectedFile = event.target.files[0];
+            setFile(selectedFile);
+            setFileName(selectedFile.name.replace(/\.[^/.]+$/, "")); // Remove file extension
+        }
+    }
+
+    async function handleUpload(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        if (!file || fileName.trim() === "") {
+            setUploadError("Please select a file and provide a name.");
+            return;
+        }
+
+        const fileExtension = file.name.split(".").pop(); // Keep original extension
+        const newFileName = `${fileName}.${fileExtension}`;
+        //const combinedTags = [model, language, langProf, tags].filter(Boolean).join(",");
+
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("fileName", newFileName);
+        formData.append("model", model);
+        formData.append("language", language);
+        formData.append("min_proficiency", langProf);
+        formData.append("tags", tags);
+        if (typeof projectName === 'string') {
+            formData.append("project_name", projectName);
+        }
+
+        for (const [key, value] of formData.entries()) {
+            console.log(`${key}:`, value);
+          }
+
+        try {
+            const response = await fetch("http://localhost:8016/audio/uploadAudioFile", {
+                method: "POST",
+                body: formData,
+                credentials: 'include'
+            });
+
+            if (!response.ok) {
+                throw new Error("Upload failed.");
+            }
+
+            const result = await response.json();
+            console.log("Upload successful:", result);
+
+            getAudioClips()
+            setIsModalOpen(false);
+            setFile(null);
+            setFileName("");
+            setTags("");
+            setUploadError("");
+
+        } catch (error) {
+            console.error("Error uploading file:", error);
+            setUploadError("Upload failed. Please try again.");
+        }
+    }
+
+    return (
+        <RoleCheck requiredRole="researcher">
+            <div className={styles.projectBody}>
+                <Navbar />
+                <div className={styles.container}>
+                    <Sidebar project_name={projectName}></Sidebar>
+                    <div className={styles.mainContent}>
+                        <h2 className={styles.heading}>Audio Library</h2>
+
+                        {/* Metrics Section */}
+                        <div className={styles.metricsContainer}>
+                            <div className={styles.metricBox}>
+                                <h3>Total Audio Clips</h3>
+                                <p>{audioData.length}</p>
+                            </div>
+                            <div className={styles.metricBox}>
+                                <h3>Average Rating</h3>
+                                <p>{(audioData.reduce((sum, audio) => sum + audio.rating, 0) / audioData.length || 0).toFixed(2)}</p>
+                            </div>
+                            <div className={styles.metricBox}>
+                                <h3>Evaluated Clips</h3>
+                                <p>{audioData.filter(audio => audio.evaluated !== "0/50").length}</p>
+                            </div>
+                        </div>
+
+                        <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end' }}>
+                            <Button className={`${styles.startButton} ${isStarted ? styles.inProgress : ''}`} sx={{ marginLeft: "auto" }}  onClick={() => setIsStarted(!isStarted)}>
+                                {isStarted ? "In Progress" : "Start"} 
+                            </Button>
+                            <Button className={styles.addAudioBtn} sx={{ marginLeft: "20px" }}  onClick={() => setIsModalOpen(true)}> + </Button>
+                        </Box>
+
+                        <Table audioData={audioData}></Table>
+                        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} hasCloseBtn>
+                            <h2 className={styles.modalTitle}>Upload New Audio</h2>
+
+                            <form onSubmit={handleUpload}>
+                                <label className={styles.formLabel}>Choose File</label>
+                                <input 
+                                    type="file" 
+                                    accept="audio/*" 
+                                    className={styles.fileInput} 
+                                    onChange={handleFileChange} 
+                                />
+
+                            <label className={styles.formLabel}>Model</label>
+                                <input
+                                    type="text"
+                                    value={model}
+                                    onChange={(e) => setModel(e.target.value)}
+                                    className={styles.inputField}
+                                    required
+                                />
+
+                                <label className={styles.formLabel}>Language</label>
+                                    <select
+                                        value={language}
+                                        onChange={(e) => setLanguage(e.target.value)}
+                                        className={styles.inputField}
+                                        required
+                                    >
+                                        <option value="">Language</option>
+                                        {["English", "Spanish", "French", "Mandarin", "Hindi", "Arabic", "Other"].map((lang) => (
+                                        <option key={lang} value={lang}>
+                                            {lang}
+                                        </option>
+                                        ))}
+                                    </select>
+
+                                <label className={styles.formLabel}>Language Proficiency</label>
+                                    <select
+                                        value={langProf}
+                                        onChange={(e) => setLangProf(e.target.value as LangProf)}
+                                        className={styles.inputField}
+                                        required
+                                    >
+                                        {Object.values(LangProf).map((level) => (
+                                            <option key={level} value={level}>
+                                                {level.replace('_', ' ')}
+                                            </option>
+                                        ))}
+                                </select>
+
+                                <label className={styles.formLabel}>Tags (comma separated)</label>
+                                <input
+                                    type="text"
+                                    value={tags}
+                                    onChange={(e) => setTags(e.target.value)}
+                                    className={styles.inputField}
+                                />
+
+                                
+                                <button type="submit" className={styles.submitButton}>Upload</button>
+                                {uploadError && <div className={styles.invalidLabel}>{uploadError}</div>}
+                            </form>
+                        </Modal>
+                    </div>
+                </div>
+            </div>
+        </RoleCheck>
+    );
+}
