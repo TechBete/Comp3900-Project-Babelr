f2c909c5f21663728f0250c7b0ba359cc48a4036
z5420287 <z5420287@ad.unsw.edu.au>
2025-04-29 18:49:40 +1000

    moved styling into seperate folder for clip list

--- frontend/babelr/pages/Listener/clip_list.tsx
+++ frontend/babelr/pages/Listener/clip_list.tsx
@@ -1,24 +1,15 @@
 import Navbar_Listener from "components/nav_bar_listener";
 import RoleCheck from "components/role_checker";
+import styles from "stylesheets/listener_clip_list.module.css"
 
 export default function ClipList() {
     return (
+        // home page for listeners
         <RoleCheck requiredRole="listener">
             <Navbar_Listener/>
-            <div style={{display: 'flex', justifyContent: 'center' , alignItems: 'center', height: '100vh', fontWeight: 'bold'}}>
-                <button 
-                    style={{
-                        padding: '12px 24px',
-                        fontSize: '1.1rem',
-                        fontWeight: 'bold',
-                        backgroundColor: '#1976d2',
-                        color: 'white',
-                        border: 'none',
-                        borderRadius: '4px',
-                        cursor: 'pointer'
-                    }}
-                >
-                    Start Audio Review
+            <div className={styles.container}>
+                <button className={styles.startButton}>
+                        Start Audio Review
                 </button>
             </div>
         </RoleCheck>
