974856d0a1f686b25424f1eb427b0bd28a39d6c4
z5420287 <z5420287@ad.unsw.edu.au>
2025-04-29 20:56:30 +1000

    moved inline styling to seperate file for rewards shop page

--- frontend/babelr/pages/Listener/rewards_shop.tsx
+++ frontend/babelr/pages/Listener/rewards_shop.tsx
@@ -1,7 +1,6 @@
 import styles from "stylesheets/rewards_shop.module.css";
 import Navbar_Listener from "components/nav_bar_listener";
 import { useState } from "react";
-import Image from "next/image";
 import { Button } from "@mui/material";
 import RoleCheck from "components/role_checker";
 
@@ -18,9 +17,7 @@ export default function Rewards_shop() {
             })
     
             if (response.ok) {
-
-                // const response = await response.json()
-                return // change later maybe
+                return 
             } else {
                 const error = await response.json();
                 setError(error.error);
@@ -39,31 +36,23 @@ export default function Rewards_shop() {
 
     const reward_grid = test_reward_list.map((reward) => (
         <div key={reward.id} className="w-45 h-45 bg-gray-200 p-2 flex flex-col items-center justify-between rounded-lg shadow-md">
-          <Image src={reward.logo} alt={reward.title} className="w-20 h-20 object-cover rounded-md" />
-          <h2 className="text-center text-sm font-medium">{reward.title}</h2>
-          <h4 className="text-center text-sm font-medium">{reward.cost} Points</h4>
-          <Button 
-            sx={{
-            backgroundColor: "#2b4450",
-            "&:hover": {
-                backgroundColor: "#1f333c",
-            },
-            }}  
-            variant="contained" 
-            onClick={() => Redeem(reward.title)}>Redeem</Button>
+          <img src={reward.logo} alt={reward.title} className={styles.rewardImage}/>
+          <h2 className={styles.rewardTitle}>{reward.title}</h2>
+          <h4 className={styles.rewardCost}>{reward.cost} Points</h4>
+          <Button className={styles.button} onClick={() => Redeem(reward.title)}>Redeem</Button>
         </div>
     ));
-
+    
     return (
         <RoleCheck requiredRole="listener">
             <div className={styles.pageWrapper}>
                 <Navbar_Listener></Navbar_Listener>
                 <div className={styles["second_bar"]}>
-                    <h1 className="text-3xl font-bold text-center mb-4">Rewards Shop</h1>
-                    <p className=" text-center">Redeem your reward below</p>
+                    <h1 className={styles.header}>Rewards Shop</h1>
+                    <p className={styles.subText}>Redeem your reward below</p>
                 </div>
                 <div className={styles["container"]}>
-                    <div className="grid grid-cols-2 gap-40">
+                    <div className={styles.rewardsGrid}>
                     {reward_grid}
                     </div>
                 </div>
