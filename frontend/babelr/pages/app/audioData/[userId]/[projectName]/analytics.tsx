2906477f67e5ef6364eff79b1891c6ea09af0c5e
z5420287 <z5420287@ad.unsw.edu.au>
2025-04-29 18:16:43 +1000

    exported styling to another file and added comments

--- frontend/babelr/pages/app/audioData/[userId]/[projectName]/analytics.tsx
+++ frontend/babelr/pages/app/audioData/[userId]/[projectName]/analytics.tsx
@@ -6,12 +6,14 @@ import RatingsTable, { RatingsRow, RawRatingsRow } from "components/analytics/ra
 import ExportButtons from "components/analytics/export_button";
 import RatingsSummaryTable, { SummaryRowRes } from "components/analytics/rating_summary_table";
 import RoleCheck from "components/role_checker";
+import Metrics from "components/analytics/metric_picker";
 
+import { styles } from "stylesheets/analytics.module";
 import { Box, Typography, Divider} from '@mui/material';
 import { useEffect, useState } from "react";
-import Metrics from "components/analytics/metric_picker";
 import { useRouter } from "next/router";
 
+// fetching rating stats from the backend
 async function getRatingStats(projectName: string, metric: string): Promise<RatingsRow[] | null>{
   try {
     const response = await fetch('http://localhost:8016/statistics/getRatingsStats' , {
@@ -49,7 +51,7 @@ async function getRatingStats(projectName: string, metric: string): Promise<Rati
     return null;
   }
 }
-
+// fetching summary stats from the backend
 async function getSummaryStats(projectName: string): Promise<SummaryRowRes[] | null> {
   try {
     const response = await fetch('http://localhost:8016/statistics/getProjectSummary' , {
@@ -73,7 +75,7 @@ async function getSummaryStats(projectName: string): Promise<SummaryRowRes[] | n
     return null;
   }
 }
-
+// fetching graph stats from the backend
 async function getGraphStats(projectName: string): Promise<GraphStat[] | null> {
   try {
     const response = await fetch('http://localhost:8016/statistics/getGraphStats' , {
@@ -98,7 +100,6 @@ async function getGraphStats(projectName: string): Promise<GraphStat[] | null> {
   }  
 }
 
-
 export default function AnalyticsPage() {
   const [metric, setMetric] = useState<string>('');
   const [rawAudioData, setRawAudioData] = useState<RatingsRow[] | null>(null);
@@ -110,7 +111,8 @@ export default function AnalyticsPage() {
   useEffect(() => {
     if (typeof projectName !== 'string') return;
     const confirmedName: string = projectName; 
-  
+
+    // fetches data whenever metric/project name is changed
     async function fetchStats() {
       const data = await getRatingStats(confirmedName, metric);
       const summary = await getSummaryStats(confirmedName);
@@ -123,121 +125,54 @@ export default function AnalyticsPage() {
     fetchStats();
   }, [metric, projectName]);
 
+  // placeholder if issues with getting projectName
   if (typeof projectName !== 'string') {
     return <div>Loading?</div>;
   }
   
-
-    // const summaryStats: SummaryStat[] = [
-    //     // Naturalness
-    //     { clipId: 'clipA1', metric: 'Naturalness', model: 'A', mean: 4.2, std: 0.5, ci_low: 3.5, ci_high: 4.9 },
-    //     { clipId: 'clipB1', metric: 'Naturalness', model: 'B', mean: 3.7, std: 0.4, ci_low: 3.5, ci_high: 4.9 },
-    //     { clipId: 'clipC1', metric: 'Naturalness', model: 'C', mean: 3.9, std: 0.6, ci_low: 3.5, ci_high: 4.9 },
-      
-    //     // Intelligibility
-    //     { clipId: 'clipA2', metric: 'Intelligibility', model: 'A', mean: 4.5, std: 0.3, ci_low: 3.5, ci_high: 4.9 },
-    //     { clipId: 'clipB2', metric: 'Intelligibility', model: 'B', mean: 4.0, std: 0.5, ci_low: 3.5, ci_high: 4.9 },
-    //     { clipId: 'clipC2', metric: 'Intelligibility', model: 'C', mean: 4.1, std: 0.4, ci_low: 3.5, ci_high: 4.9 },
-      
-    //     // Clarity
-    //     { clipId: 'clipA3', metric: 'Clarity', model: 'A', mean: 4.3, std: 3, ci_low: 3.5, ci_high: 4.9 },
-    //     { clipId: 'clipB3', metric: 'Clarity', model: 'B', mean: 3.9, std: 0.6, ci_low: 3.5, ci_high: 4.9 },
-    //     { clipId: 'clipC3', metric: 'Clarity', model: 'C', mean: 4.0, std: 0.5, ci_low: 3.5, ci_high: 4.9 },
-      
-    //     // New Metric
-    //     { clipId: 'clipA4', metric: 'new metrics', model: 'A', mean: 3.3, std: 3.4, ci_low: 3.5, ci_high: 4.9 },
-    //     { clipId: 'clipB4', metric: 'new metrics', model: 'B', mean: 3.9, std: 3.6, ci_low: 3.5, ci_high: 4.9 },
-    //     { clipId: 'clipC4', metric: 'new metrics', model: 'C', mean: 3.0, std: 2.5, ci_low: 3.5, ci_high: 4.9 },
-    //   ];
-
   return (
     <RoleCheck requiredRole="researcher">
       <Navbar />
-      <Box sx={{ display: "flex", backgroundColor: "#DFEBED", minHeight: "100vh"}}>
+      <Box sx={styles.pageContainer}>
         <Sidebar project_name={projectName} />
 
-        <Box sx={{ flexGrow: 1, overflowX:'hidden', display: "flex", flexDirection: "column", mt: "50px", p: 3, gap: 4 }}>
-
-          <Typography variant="h4" gutterBottom>Analytics Dashboard</Typography>
-          {/* Top Section */}
+        <Box sx={styles.contentContainer}>
+          <Typography variant="h4" sx={styles.heading}>
+            Analytics Dashboard
+          </Typography>
+          {/* Top Section - Summary and demogrpahics */}
           <Box sx={{ display: "flex", gap: 2 }}>
-            <Box sx={{
-              flexGrow:1,
-              border: '2px solid #f57c00',
-              borderRadius: 2,
-              p: 2,
-              backgroundColor: '#fff3e0',
-              boxShadow: 2,
-              }}>
-              <Typography variant="h6" sx={{ mb: 1 }}>Summary Statistics</Typography>
+            {/* Summary Box */}
+            <Box sx={styles.summaryBox}>
+              <Typography variant="h6" sx={styles.subHeading}>
+                Summary Statistics
+              </Typography>
               <Divider sx={{ mb: 2 }} />
               {summaryData? <RatingsSummaryTable data={summaryData}/>: <div>No Data</div>}
             </Box>
-            
-            {/* <Box sx={{
-              flexGrow: 1,
-              border: '2px solid #1976d2',
-              borderRadius: 2,
-              p: 2,
-              backgroundColor: '#e3f2fd',
-              boxShadow: 2,
-            }}>
-              {summaryData? <RatingChart data={summaryStats}/>: <div>No Data</div> }
-            </Box> */}
-
-            <Box sx={{
-              width: 300,
-              border: '2px solid #1976d2',
-              borderRadius: 2,
-              p: 2,
-              backgroundColor: '#e3f2fd',
-              boxShadow: 2,
-            }}>
+            {/* Demographics Box*/}
+            <Box sx={styles.demographicsBox}>
               <DemographicsBox projectName={projectName} />
             </Box>
           </Box>
 
-          {/* Middle Section - Summary Stats */}
-          <Box sx={{ flexGrow: 1, border: '2px solid #1976d2', borderRadius: 2, p: 2, backgroundColor: '#e3f2fd', boxShadow: 2 }}>
+          {/* Middle Section - Summary Stats (displaying as a graph) */}
+          <Box sx={styles.graphBox}>
             {graphData ? <RatingChart data={graphData} /> : <div>No data</div>}
           </Box>
-
           <Metrics onMetricSelect={(metric: string) => setMetric(metric)}/>
-          {/* <Typography variant="h4" gutterBottom>Metric is {metric}</Typography> */}
 
-          {/* Bottom Section - Raw Data */}
-          <Box sx={{
-            border: '2px solid #388e3c',
-            borderRadius: 2,
-            p: 2,
-            backgroundColor: '#e8f5e9',
-            boxShadow: 2,
-            maxWidth: '100%',
-            overflowX: 'auto'
-          }}>
+          {/* Bottom Section - Raw Data Output */}
+          <Box sx={styles.tableBox}>
             <Typography variant="h6" sx={{ mb: 1 }}>{metric} Data</Typography>
             <Divider sx={{ mb: 2 }} />
             {rawAudioData ? <RatingsTable data={rawAudioData} /> : <div>No Data</div>}
           </Box>
 
-          {/* Bottom Section - Summary Stats */}
-          {/* <Box sx={{
-            border: '2px solid #f57c00',
-            borderRadius: 2,
-            p: 2,
-            backgroundColor: '#fff3e0',
-            boxShadow: 2,
-          }}>
-            <Typography variant="h6" sx={{ mb: 1 }}>Summary Statistics</Typography>
-            <Divider sx={{ mb: 2 }} />
-            <RatingsSummaryTable />
-          </Box> */}
-
           {/* Export Buttons */}
-          <Box sx={{ mt: 2 }}>
+          <Box sx={styles.exportBox}>
             <ExportButtons />
           </Box>
-
         </Box>
       </Box>
     </RoleCheck>
